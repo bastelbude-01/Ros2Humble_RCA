91c86e53500d5e18def8b5178ce94bd2f2e0dbf3
bastelbude-01 <mod3llfr3ak1mg3ist@web.de>
2024-01-16 22:12:59 +0100

    controller works

--- rca_controller/launch/controller.launch.py
+++ rca_controller/launch/controller.launch.py
@@ -10,6 +10,13 @@ from launch.conditions import UnlessCondition
 
 def generate_launch_description():
 
+    is_sim_arg = DeclareLaunchArgument(
+        "is_sim",
+        default_value="True"
+    )
+
+    is_sim = LaunchConfiguration("is_sim")
+
     robot_description = ParameterValue(
         Command(
             [
@@ -18,23 +25,33 @@ def generate_launch_description():
                     get_package_share_directory("rca_description"),
                     "urdf",
                     "rca.urdf.xacro",
-                ),
+                )," is_sim:=False"
             ]
         ),
         value_type=str,
-    )    
+    )
+
+    robot_state_publisher = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[{"robot_description": robot_description}],
+        condition=UnlessCondition(is_sim),
+    )
 
     controller_manager = Node(
         package="controller_manager",
         executable="ros2_control_node",
         parameters=[
-            {"robot_description": robot_description},
+            {"robot_description": robot_description,
+            "use_sim_time": is_sim
+             },
             os.path.join(
                 get_package_share_directory("rca_controller"),
                 "config",
                 "rca_controllers.yaml",
             ),
         ],
+        condition=UnlessCondition(is_sim),
     )
 
     joint_state_broadcaster_spawner = Node(
@@ -53,18 +70,18 @@ def generate_launch_description():
         arguments=["arm_controller",
                    "--controller-manager", "/controller_manager"],
     )
+
     gripper_controller_spawner = Node(
         package="controller_manager",
         executable="spawner",
         arguments=["gripper_controller",
                    "--controller-manager", "/controller_manager"],
     )
-    
-
-    
 
     return LaunchDescription(
-        [    
+        [   
+            is_sim_arg,
+            robot_state_publisher,
             controller_manager,
             joint_state_broadcaster_spawner,
             arm_controller_spawner,
